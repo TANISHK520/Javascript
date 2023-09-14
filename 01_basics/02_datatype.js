0d7ea9f884afbcd222a9e1c0fadfb35534620402
Tanish Kapoor <68555885+TANISHK520@users.noreply.github.com>
2023-09-14 10:32:25 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; /*it means treat all js code as newer version of js*/
+
+// alert(3+3); /*we are using node js not browser*/
+
+console.log(3+3);
+
+let name="tanish"
+let age=22
+let isLogIn=false
+let state; /*undefined*/
+
+/*
+DATATYPES
+Number
+BigInt
+string=""
+boolean:true/false
+null:standalone value
+undefined
+symbol
+*/
+console.log(typeof 23);
+console.log(typeof "kapoor");
+/*#**INTERVIEW QUESTION TYPE OF NULL***/
+console.log(typeof null);  /*object*/
+console.log(typeof undefined); /*undefined*/
