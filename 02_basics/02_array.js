f024ce1a2ffc95bd4a8046e440559bb08a73e073
Tanish Kapoor <68555885+TANISHK520@users.noreply.github.com>
2023-11-13 10:42:20 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -2,13 +2,15 @@ const marvel_heroes=["thor","ironman","spiderman"]
 const dc_heroes=["superman","flash","batman"]
 
 
-/*
-push do stuff on exixting array
+let nwarr=marvel_heroes.concat(dc_heroes)
+console.log(nwarr)
+
+// push do stuff on exixting array
 marvel_heroes.push(dc_heroes)
 console.log(marvel_heroes);
 console.log(marvel_heroes[3]);
 console.log(marvel_heroes[3][0]);
-*/
+
 
 
 
@@ -23,6 +25,22 @@ const all_NewHeroes=[...marvel_heroes,...dc_heroes]
 console.log(all_NewHeroes);
 
 
+
+
+
+
+const ar1=[1,2,3]
+
+const ar2=[4,5,6]
+
+const ar3=[7,8,9]
+
+const ne=[...ar1,...ar2,...ar3]
+console.log(ne)
+
+
+
+
 const another_array=[1,2,3,[4,5,6],7,[6,7],[4,5]]
 const useable_another_array=another_array.flat(Infinity) /*infinity is depth here  */
 console.log(useable_another_array);
@@ -34,14 +52,18 @@ console.log(Array.from("Tanish"));
 console.log(Array.from({name:"tanish"})); /*specify what to array*/
 
 
+
+
 /*important*/
 let score1=100
 let score2=109;
 let score3=55
 console.log(Array.of(score1,score2,score3));
 
-
-
+let s="tanish"
+let p="kapoor"
+let q=Array.of(s,p)
+console.log(q)
 
 
 
