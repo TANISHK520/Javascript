eef00f7f56352a26f49669cf5bcfa4369d717ef9
Tanish Kapoor <68555885+TANISHK520@users.noreply.github.com>
2023-11-13 18:44:06 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,49 @@
+
+function sayMyName(){
+    console.log("T")
+    console.log("A")
+    console.log("N")
+    console.log("I")
+    console.log("S")
+    console.log("H")
+}
+
+sayMyName()
+
+
+
+
+                        // parameters
+                            // |
+function addTwoNumber(number1,number2){
+    
+    console.log(number1+number2)
+    let result=number1+number2
+    return result
+}
+addTwoNumber(3,4)
+            // |
+            //   arguments
+
+const result=addTwoNumber(3,"2")
+console.log(`Result is ${result}`)
+
+
+
+
+
+function loginUserMessage(username="sam"){
+    // !username is equivalent to username==undefined , !username used in industry !undefined means true krdo then go to if block
+    if(username==undefined){
+        console.log("enter username")
+        return
+    }
+    return `${username} just loggedin`
+}
+
+console.log(loginUserMessage("tanish"))
+
+// jab kuch pass nahi kia assigned by undefined
+console.log(loginUserMessage())
+// note:::
+// if username="sam" , if pass nothing then print sam just loggedin . if wala never get executed
