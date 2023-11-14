5c98da031d542161410c74a26d8e68f21cae3e62
Tanish Kapoor <68555885+TANISHK520@users.noreply.github.com>
2023-11-14 04:45:27 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,11 +1,17 @@
 // let var const
 // use let and const support scope
+
+
+
+
+// two ways of declaring function.
+// 1.can call before declare work fine
 console.log(addone(2))
 function addone(num1){
     return num1+1
 }
 
-
+// 2.cant call befroe Declaration
 // console.log(addtwo(2)) yaha error aayga.
 const addtwo=function(num){
     return num+2
