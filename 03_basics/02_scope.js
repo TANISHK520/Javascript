7ff2d59a39291f51d2c60464c18f90b09ecb580b
Tanish Kapoor <68555885+TANISHK520@users.noreply.github.com>
2023-11-14 04:44:01 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,2 +1,13 @@
 // let var const
 // use let and const support scope
+console.log(addone(2))
+function addone(num1){
+    return num1+1
+}
+
+
+// console.log(addtwo(2)) yaha error aayga.
+const addtwo=function(num){
+    return num+2
+}
+console.log(addtwo(2))
